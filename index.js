d8ff79ec428c21c68853966638e739bd03283d3d
nick <nickorsk2017@gmail.com>
2016-10-19 21:03:37 +0500

    fixing errors building package

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.Form = undefined;
+
+var _Form2 = require('./lib/Form');
+
+var _Form3 = _interopRequireDefault(_Form2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Form = _Form3.default;
